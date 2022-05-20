2d215bd02bf0fbf16541e1b87839bdf0d79433ca
Tom Gregor <tgregor@ivsoftware.com>
2022-05-20 08:33:52 -0600

    Add project files.

---
+++ sqlite_hello_world/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+namespace sqlite_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "sqlite_hello_world");
+            Directory.CreateDirectory(appData);
+            var connectionString = Path.Combine(appData, "MyDatabase.db");
+
+            using (var cnx = new SQLiteConnection(connectionString))
+            {
+                cnx.CreateTable<Record>();
+
+                var newRecord = new Record { Description = "Hello" };
+
+                var guidToFind = newRecord.guid;
+
+                cnx.Insert(newRecord);
+
+                var recordset = cnx.Query<Record>($"SELECT * FROM items WHERE guid = '{guidToFind}'");
+
+                Console.WriteLine($"Found: {recordset[0].Description}");
+            }
+
+            Console.ReadKey();
+        }
+
+        [Table("items")]
+        class Record
+        {
+            [PrimaryKey]
+            public string guid { get; set; } = Guid.NewGuid().ToString().Trim().TrimStart('{').TrimEnd('}');
+            public string Description { get; set; } = string.Empty;
+        }
+    }
+}
